a2087bc4662c7b9002e5feedbb644705e2ca8d99
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-06-17 11:37:45 +0530

    Update data_collection.py

--- src/data_collection.py
+++ src/data_collection.py
@@ -4,8 +4,6 @@ import os
 from sklearn.model_selection import train_test_split
 import yaml
 
-
-
 #df = pd.read_csv(r"C:\Users\Sande\Desktop\Datasets\medical_insurance.csv")
 
 #test_size = yaml.safe_load(open("params.yaml"))['data_collection']['test_size']
